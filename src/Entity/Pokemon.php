6fe496caa4ba6db0175e2e7db6a162cd292e22cf
aymen04 <aymenadline5@gmail.com>
2022-11-28 14:09:22 +0100

    Last commit

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $number = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Types = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getTypes(): ?string
+    {
+        return $this->Types;
+    }
+
+    public function setTypes(string $Types): self
+    {
+        $this->Types = $Types;
+
+        return $this;
+    }
+}
